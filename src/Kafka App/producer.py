f2c14f280ed01baee763b333fa9437a6dc8105ec
ugurcankok <ugurcankok0@gmail.com>
2023-08-13 22:52:47 +0300

    Send message to single partition

--- src/Kafka App/producer.py
+++ src/Kafka App/producer.py
@@ -4,8 +4,12 @@ from data import get_registed_user
 def json_serializer(data):
     return json.dumps(data).encode("utf-8")
 
+def get_partition(key, all, available):
+    return 0
+
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=json_serializer)
+                         value_serializer=json_serializer,
+                         partitioner=get_partition)
 
 
 if __name__ == "__main__":
