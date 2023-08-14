862f14c36de1212e680103956e4b1e21d16ac170
ugurcankok <ugurcankok0@gmail.com>
2023-08-14 09:12:18 +0300

    Add consumer

---
+++ src/Kafka Demo/consumer.py
@@ -0,0 +1,15 @@
+from kafka import KafkaConsumer
+import json
+
+if __name__ == "__main__":
+    consumer = KafkaConsumer(
+        "registered_user",
+        bootstrap_servers='localhost:9092',
+        auto_offset_reset="earliest",
+        group_id="consumer-group-a"
+    )
+
+    print("starting the consumer")
+
+    for msg in consumer:
+        print(f"Registered User = {json.loads(msg.value)}")
