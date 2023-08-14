5b29847a07466f98a70c7953712fca6c0982d741
ugurcankok <ugurcankok0@gmail.com>
2023-08-14 09:10:52 +0300

    Update producer

--- src/Kafka Demo/producer.py
+++ src/Kafka Demo/producer.py
@@ -4,12 +4,16 @@ from data import get_registed_user
 def json_serializer(data):
     return json.dumps(data).encode("utf-8")
 
-def get_partition(key, all, available):
+# if you have two partitions, you will use P0
+'''def get_partition(key, all, available):
     return 0
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=json_serializer,
-                         partitioner=get_partition)
+                         partitioner=get_partition)'''
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=json_serializer)
 
 
 if __name__ == "__main__":
